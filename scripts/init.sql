d1b21cc36dc61569151253fb5deeb5a0adbe019a
Rafael Gomes <rafael.gomes@sou.inteli.edu.br>
2025-05-06 12:08:26 -0300

    first commit

---
+++ scripts/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS aluno (
+  id SERIAL PRIMARY KEY,
+  nome TEXT NOT NULL,
+  email TEXT NOT NULL,
+  criado_em TIMESTAMP DEFAULT NOW()
+);
+
+CREATE INDEX IF NOT EXISTS idx_aluno_email ON aluno (email);
